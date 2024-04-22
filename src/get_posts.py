8debd7e342b57bb10f7f831ac5d4932dc85d3076
Billy <billy@mail.com>
2024-04-22 12:06:59 -0400

    Update

--- src/get_posts.py
+++ src/get_posts.py
@@ -1,29 +1,32 @@
 from typing import List
 from requests import get
-from get_data import get_data
+from src.get_data import get_data_from_username
 
 
 def get_posts(username: str) -> List:
-    ID = get_data(username)["mainEntity"]["identifier"]
-    data_total = []
-    url = "https://imginn.com/api/posts/"
+    try:
+        ID = get_data_from_username(username)["mainEntity"]["identifier"]
+        data_total = []
+        url = "https://imginn.com/api/posts/"
 
-    querystring = {"id": f"{ID}", "username": f"{username}", "cursor": f"_{ID}"}
+        querystring = {"id": f"{ID}", "username": f"{username}", "cursor": f"_{ID}"}
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 12; M2101K7BNY Build/SP1A.210812.016) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 GNews Android/2022120654"
-    }
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 12; M2101K7BNY Build/SP1A.210812.016) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 GNews Android/2022120654"
+        }
 
-    has_next = True
+        has_next = True
 
-    while has_next:
-        response = get(url, headers=headers, params=querystring, timeout=60)
-        data = response.json()
-        data_total.append(data)
+        while has_next:
+            response = get(url, headers=headers, params=querystring, timeout=60)
+            data = response.json()
+            data_total.append(data)
 
-        has_next = data.get("hasNext", False)
+            has_next = data.get("hasNext", False)
 
-        if has_next:
-            querystring["cursor"] = data["cursor"]
+            if has_next:
+                querystring["cursor"] = data["cursor"]
 
-    return data_total
+        return data_total
+    except Exception as e:
+        return str(e)
