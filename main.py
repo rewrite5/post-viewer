1b754c12daf8ea2b4b19d561a29befb29b5b0796
Billy <billy@mail.com>
2024-04-22 16:40:36 -0400

    Update

--- main.py
+++ main.py
@@ -1,8 +1,75 @@
+# from src.get_posts import get_posts
+# from src.get_data import get_data_from_username
+# from pydantic import BaseModel
+# from fastapi import FastAPI, Query
+# # import uvicorn
+
+# app = FastAPI()
+
+
+# class About(BaseModel):
+#     title: str
+#     description: str
+
+
+# class Msg(BaseModel):
+#     msg: str
+
+
+# @app.get("/")
+# async def get_index():
+#     msg = Msg(msg="Welcome!")
+#     return msg
+
+
+# @app.get("/api/user/")
+# async def validate_user(username: str = Query(..., description="Username of the user")):
+#     try:
+#         user = get_data_from_username(username)
+#         return [user]
+#     except Exception as e:
+#         return {"error": str(e)}
+
+
+# @app.get("/api/posts/")
+# async def get_user_posts(
+#     username: str = Query(..., description="Username of the user")
+# ):
+#     try:
+#         data_total = get_posts(username)
+#         if isinstance(data_total, str):
+#             return {"error": "User not found"}
+#         return data_total
+#     except Exception as e:
+#         return {"error": str(e)}
+
+
+# @app.get("/api/stories/")
+# async def get_user_stories(
+#     # username: str = Query(..., description="Username of the user")
+# ):
+#     msg = Msg(msg="Under construction")
+#     return msg
+
+
+# @app.get("/api/about/")
+# async def get_about():
+#     about = About(
+#         title="About instaPy",
+#         description="Site to view content such as: photos, videos, reels, instagram anonymously XD!",
+#     )
+#     return about
+
+
+# if __name__ == "__main__":
+#     pass
+
+
+from typing import List
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
 from src.get_posts import get_posts
 from src.get_data import get_data_from_username
-from pydantic import BaseModel
-from fastapi import FastAPI, Query
-# import uvicorn
 
 app = FastAPI()
 
@@ -17,49 +84,48 @@ class Msg(BaseModel):
 
 
 @app.get("/")
-async def get_index():
+async def get_index() -> Msg:
     msg = Msg(msg="Welcome!")
     return msg
 
 
 @app.get("/api/user/")
-async def validate_user(username: str = Query(..., description="Username of the user")):
+async def validate_user(
+    username: str = Query(..., description="Username of the user")
+) -> List[dict]:
     try:
         user = get_data_from_username(username)
         return [user]
     except Exception as e:
-        return {"error": str(e)}
+        return [{"error": str(e)}]
 
 
 @app.get("/api/posts/")
 async def get_user_posts(
     username: str = Query(..., description="Username of the user")
-):
+) -> List[dict]:
     try:
         data_total = get_posts(username)
         if isinstance(data_total, str):
-            return {"error": "User not found"}
+            return [{"error": "User not found"}]
         return data_total
     except Exception as e:
-        return {"error": str(e)}
+        return [{"error": str(e)}]
 
 
 @app.get("/api/stories/")
-async def get_user_stories(
-    # username: str = Query(..., description="Username of the user")
-):
+async def get_user_stories() -> Msg:
     msg = Msg(msg="Under construction")
     return msg
 
 
 @app.get("/api/about/")
-async def get_about():
+async def get_about() -> About:
     about = About(
         title="About instaPy",
         description="Site to view content such as: photos, videos, reels, instagram anonymously XD!",
     )
     return about
-
 
 if __name__ == "__main__":
     pass
--- src/get_data.py
+++ src/get_data.py
@@ -1,9 +1,41 @@
+# from bs4 import BeautifulSoup
+# from requests import get
+# from json import loads
+
+
+# def get_data_from_username(username: str) -> str:
+#     try:
+#         url = f"https://imginn.com/{username}"
+#         headers = {
+#             "User-Agent": "Mozilla/5.0 (Linux; Android 12; M2101K7BNY Build/SP1A.210812.016) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 GNews Android/2022120654"
+#         }
+#         response = get(url, headers=headers, timeout=60)
+
+#         if response.status_code == 200:
+#             soup = BeautifulSoup(response.text, "html.parser")
+#             script_tag = (
+#                 soup.find("script", type="application/ld+json")
+#                 .text.replace("\n", "")
+#                 .replace("\r", "")
+#             )
+
+#             data = loads(script_tag)
+#             return data
+#         elif response.status_code == 404:
+#             return "User not found"
+#         else:
+#             return "Error in the request"
+#     except Exception as e:
+#         return str(e)
+
+
 from bs4 import BeautifulSoup
 from requests import get
 from json import loads
+from typing import Union
 
 
-def get_data_from_username(username: str) -> str:
+def get_data_from_username(username: str) -> Union[str, dict]:
     try:
         url = f"https://imginn.com/{username}"
         headers = {
