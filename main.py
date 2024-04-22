a734292ed9ec3a67cf6d0b403e8e97df84ca07c8
Billy <billy@mail.com>
2024-04-22 16:07:55 -0400

    Update

--- main.py
+++ main.py
@@ -1,111 +1,65 @@
-# from src.get_posts import get_posts
-# from pydantic import BaseModel
-# from fastapi import FastAPI, Query
-# # import uvicorn
-
-# app = FastAPI()
-
-
-# class About(BaseModel):
-#     title: str
-#     description: str
-
-
-# class Msg(BaseModel):
-#     msg: str
-
-
-# @app.get("/")
-# async def get_index():
-#     msg = Msg(msg="Welcome!")
-#     return msg
-
-
-# @app.get("/api/posts/")
-# async def get_user_posts(
-#     username: str = Query(..., description="Username of the user")
-# ):
-#     try:
-#         data_total = get_posts(username)
-#         if isinstance(data_total, str):
-#             return {"error": "User not found"}
-#         return data_total
-#     except Exception as e:
-#         return {"error": str(e)}
-
-
-# @app.get("/api/stories/")
-# async def get_user_stories(
-#     # username: str = Query(..., description="Username of the user")
-# ):
-#     msg = Msg(msg="Under construction")
-#     return msg
-
-
-# @app.get("/api/about/")
-# async def get_about():
-#     about = About(
-#         title="About instaPy",
-#         description="Site to view content such as: photos, videos, reels, instagram anonymously XD!",
-#     )
-#     return about
-
-
-# if __name__ == "__main__":
-#     pass
-
-
-from flask import Flask, request, jsonify
 from src.get_posts import get_posts
+from src.get_data import get_data_from_username
+from pydantic import BaseModel
+from fastapi import FastAPI, Query
+# import uvicorn
 
-app = Flask(__name__)
-
-
-class About:
-    def __init__(self, title, description):
-        self.title = title
-        self.description = description
+app = FastAPI()
 
 
-class Msg:
-    def __init__(self, msg):
-        self.msg = msg
+class About(BaseModel):
+    title: str
+    description: str
 
 
-@app.route("/")
-def get_index():
-    msg = Msg("¡Bienvenido!")
-    return jsonify(msg.__dict__)
+class Msg(BaseModel):
+    msg: str
 
 
-@app.route("/api/posts/")
-def get_user_posts():
-    username = request.args.get("username")
-    if not username:
-        return jsonify({"error": "Nombre de usuario no proporcionado"})
+@app.get("/")
+async def get_index():
+    msg = Msg(msg="Welcome!")
+    return msg
+
+
+@app.get("/api/user/")
+async def validate_user(username: str = Query(..., description="Username of the user")):
+    try:
+        user = get_posts(username)
+        return user
+    except Exception as e:
+        return {"error": str(e)}
+
+
+@app.get("/api/posts/")
+async def get_user_posts(
+    username: str = Query(..., description="Username of the user")
+):
     try:
         data_total = get_posts(username)
         if isinstance(data_total, str):
-            return jsonify({"error": "Usuario no encontrado"})
-        return jsonify(data_total)
+            return {"error": "User not found"}
+        return data_total
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return {"error": str(e)}
 
 
-@app.route("/api/stories/")
-def get_user_stories():
-    msg = Msg("En construcción")
-    return jsonify(msg.__dict__)
+@app.get("/api/stories/")
+async def get_user_stories(
+    # username: str = Query(..., description="Username of the user")
+):
+    msg = Msg(msg="Under construction")
+    return msg
 
 
-@app.route("/api/about/")
-def get_about():
+@app.get("/api/about/")
+async def get_about():
     about = About(
-        title="Acerca de instaPy",
-        description="¡Sitio para ver contenido como: fotos, videos, reels, Instagram de forma anónima XD!",
+        title="About instaPy",
+        description="Site to view content such as: photos, videos, reels, instagram anonymously XD!",
     )
-    return jsonify(about.__dict__)
+    return about
 
 
 if __name__ == "__main__":
-    app.run()
+    pass
